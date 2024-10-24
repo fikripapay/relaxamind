558e3409bdd4aedb35e90cf1975b36440a578b11
Abdul Fikri <fikriabdul679@gmail.com>
2024-10-24 15:41:23 +0700

    update

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 <body>
 
     <!-- Start Navbar -->
-    <nav class="navbar navbar-dark navbar-expand-lg sticky-top border-bottom" style="background: linear-gradient(to right, #ffffff, #A3C1DA);">
+    <nav class="navbar navbar-dark navbar-expand-lg sticky-top border-bottom" style="background-color: #005f73;">
         <div class="container">
             <a class="navbar-brand d-flex align-items-center" href="index.php" data-aos="fade-down" data-aos-once="true"
                 data-aos-duration="1000">
@@ -61,13 +61,13 @@
                         </a>
                         <ul class="dropdown-menu">
                             <li>
-                            <a class="dropdown-item my-dropdown" style="color: #071952;" href="#"
+                            <a class="dropdown-item my-dropdown" style="color: #005f73;" href="#"
                                 >Edukasi</a
                             >
                             </li>
                             <li>
-                            <a class="dropdown-item my-dropdown" style="color: #071952;" href="#"
-                                >Tes Stress</a
+                            <a class="dropdown-item my-dropdown" style="color: #005f73;" href="#"
+                                >Tes Stres</a
                             >
                             </li>
                         </ul>
@@ -84,30 +84,32 @@
                         </a>
                         <ul class="dropdown-menu">
                             <li>
-                            <a class="dropdown-item my-dropdown" style="color: #071952;" href="#"
+                            <a class="dropdown-item my-dropdown" style="color: #005f73;" href="#"
                                 >Journaling</a
                             >
                             </li>
                             <li>
-                            <a class="dropdown-item my-dropdown" style="color: #071952;" href="#"
+                            <a class="dropdown-item my-dropdown" style="color: #005f73;" href="#"
                                 >Meditasi (Video)</a
                             >
                             </li>
                             <li>
-                            <a class="dropdown-item my-dropdown" style="color: #071952;" href="#"
+                            <a class="dropdown-item my-dropdown" style="color: #005f73;" href="#"
                                 >Musik Relaksasi</a
                             >
                             </li>
                             <li>
-                            <a class="dropdown-item my-dropdown" style="color: #071952;" href="#"
+                            <a class="dropdown-item my-dropdown" style="color: #005f73;" href="#"
                                 >Tips Harian</a
                             >
                             </li>
                         </ul>
                     </li>
-                    <a class="nav-link text-white" href="#">Progress</a>
+                    <a class="nav-link text-white" href="#">Perkembangan</a>
                     <a class="nav-link text-white" href="#">Artikel</a>
                     <a class="nav-link text-white" href="#">Konseling</a>
+                    <a class="nav-link ms-lg-3 mt-3 mt-lg-0 text-center rounded text-white" href="info-ppdb.php" style="background-color: #28a745";>Daftar</a>
+                    <a class="nav-link ms-lg-3 mt-3 mt-lg-0 text-center rounded text-white" href="info-ppdb.php" style="background-color: #007bff";>Masuk</a>
                 </div>
             </div>
         </div>
@@ -115,73 +117,17 @@
     <!-- End Navbar -->
 
     <!-- Start Hero Section -->
-<section id="hero" class="hero" data-aos="zoom-in" data-aos-once="true" data-aos-duration="1000">
-        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <?php
-        include "koneksi.php";
-
-        $query_cek = "SELECT COUNT(*) AS jumlah FROM tbl_banner";
-        $result_cek = mysqli_query($conn, $query_cek);
-        $row_cek = mysqli_fetch_assoc($result_cek);
-        $jumlah_banner = $row_cek['jumlah'];
-
-        // Jika jumlah banner dengan nama yang sama lebih dari 0, berikan pesan kesalahan
-        if ($jumlah_banner > 0) {
-
-          for ($i = 0; $i < $jumlah_banner; $i++) {
-            $slide_number = $i + 1;
-            $aria_label = "Slide " . $slide_number;
-            $class_active = ($i == 0) ? "active" : "";
-        ?>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="<?php echo $i; ?>"
-                    class="<?php echo $class_active; ?>" aria-current="<?php echo $class_active; ?>"
-                    aria-label="<?php echo $aria_label; ?>"></button>
-                <?php
-          }
-        }
-        ?>
-            </div>
-
-
-            <div class="carousel-inner">
-
-                <?php
-        $sql = "SELECT * FROM tbl_banner";
-        // Eksekusi query
-        $result = $conn->query($sql);
-        // Loop melalui hasil query dan tampilkan dalam tabel
-        if ($result->num_rows > 0) {
-          while ($row = $result->fetch_assoc()) {
-        ?>
-                <div class="carousel-item active">
-                    <img src="assets/img/hero/<?php echo $row['nama'] ?>.png" class="d-block w-100" alt="Banner" />
-                </div>
-                <?php
-          }
-        }
-        ?>
-            </div>
+    <section id="hero" data-aos="zoom-in" data-aos-once="true" data-aos-duration="1000">
+        <div class="container text-center">
+            <h1>Apakah Anda Merasa Stres?</h1>
+            <p>Tes ini dirancang untuk membantu Anda mengetahui tingkat stres Anda dan memberikan saran untuk mengelolanya, Dapatkan hasil yang akurat dan tips praktis untuk mengelola stres dalam keseharian Anda.</p>
+            <a class="btn btn-primary hu__hu__" href="#" role="button">Mulai Tes Sekarang</a>
         </div>
     </section>
     <!-- End Hero Section -->
 
-    <!-- Start Intro Video -->
-    <section id="introsekolah" class="mt-5">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-12 col-lg-10">
-                    <video class="w-100 rounded" controls data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-sine" data-aos-delay="500">
-                        <source src="assets/video/intro.mp4" type="video/mp4">
-                    </video>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- End Intro Video -->
-
     <!-- Start Jurusan Section -->
-    <section id="jurusan" class="jurusan mt-5 py-5">
+    <section id="jurusan" class="jurusan py-5">
         <div class="container text-center">
             <h2 class="judul" data-aos="fade-up" data-aos-duration="1000">
                 JURUSAN
@@ -260,6 +206,9 @@
                 <!-- BERITA -->
 
                 <?php
+        include "koneksi.php";
+        ?>
+                <?php
         $sql = "SELECT * FROM tb_berita ORDER BY id DESC LIMIT 6";
         // Eksekusi query
         $result = $conn->query($sql);
@@ -291,53 +240,10 @@
     </section>
     <!-- End Berita Section -->
 
-    <!-- Start Kontak Section -->
-    <section id="kontak" class="kontak">
-        <div class="container">
-            <div class="row py-5 justify-content-sm-between">
-                <div class="col-md-2">
-                    <h3>Ikuti Kami</h3>
-                    <div class="followme">
-                        <a href="https://www.instagram.com/smktonjong/" target="_blank"><i
-                                class="fa-brands fa-square-instagram"></i></a>
-                        <a href="https://www.youtube.com/@smkstonjong6210" target="_blank"><i class="fa-brands fa-square-youtube"></i></a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <h3>Hubungi Kami</h3>
-                    <div class="detail">
-                        <i class="fa-solid fa-phone"></i>
-                        <span>(0251) 8583881</span>
-                    </div>
-                    <div class="detail mt-3">
-                        <a href="https://wa.me/6285772226202" target="_blank">
-                            <i class="fa-brands fa-whatsapp"></i>
-                            +62 857-7222-6602
-                        </a>
-                    </div>
-                    <div class="detail mt-3">
-                        <a href="mailto:smktonjong@gmail.com" target="_blank">
-                            <i class="fa-regular fa-envelope"></i>
-                            smktonjong@gmail.com</a>
-                    </div>
-                </div>
-                <div class="col-md-5">
-                    <h3>Lokasi</h3>
-                    <div class="lokasi">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7928.424564449169!2d106.75924559127593!3d-6.494785423989194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c2f654a8a85b%3A0x51cf6d4a78fe70fc!2sYayasan%20Dharma%20Bakti%20SMK%20TONJONG!5e0!3m2!1sid!2sid!4v1712673758098!5m2!1sid!2sid"
-                            allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- End Kontak Section -->
-
     <!-- Start Footer -->
     <footer class="footer text-center">
         <p class="text-white">
-            Copyright &copy; 2024 SMK Tonjong. All Rights Reserved.
+            Copyright &copy; 2024 RelaxaMind. All Rights Reserved.
         </p>
     </footer>
     <!-- End Footer -->
